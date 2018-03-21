60fa42f78d9569dec3ac6b4bd84e2344c733d9b6
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-21 15:55:50 -0700

    Add nox tests for pkgutil

--- nox.py
+++ nox.py
@@ -1,9 +1,19 @@
 import nox
 
+
 @nox.session
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def tests(session, interpreter):
+def test_native(session, interpreter):
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
     session.install('native/a/', 'native/b/', 'native/b-c/')
+    session.run('pytest')
+
+
+@nox.session
+@nox.parametrize('interpreter', ('python2', 'python3'))
+def test_pkgutil(session, interpreter):
+    session.interpreter = interpreter
+    session.install('-r', 'tests/requirements.txt')
+    session.install('pkgutil/a/', 'pkgutil/b/', 'pkgutil/b-c/')
     session.run('pytest')
