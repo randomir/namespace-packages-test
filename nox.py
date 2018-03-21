d8ba79ff7751228833fbc26de93819848588f203
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-21 16:41:39 -0700

    Add tests for different pkg levels

--- nox.py
+++ nox.py
@@ -12,8 +12,26 @@ def test_native(session, interpreter):
 
 @nox.session
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_pkgutil(session, interpreter):
+def test_pkgutil_mixed_levels(session, interpreter):
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
     session.install('pkgutil/a/', 'pkgutil/b/', 'pkgutil/b-c/')
     session.run('pytest')
+
+
+@nox.session
+@nox.parametrize('interpreter', ('python2', 'python3'))
+def test_pkgutil_same_level_only(session, interpreter):
+    session.interpreter = interpreter
+    session.install('-r', 'tests/requirements.txt')
+    session.install('pkgutil/a/', 'pkgutil/b/')
+    session.run('pytest', 'tests/test_a.py', 'tests/test_b.py')
+
+
+@nox.session
+@nox.parametrize('interpreter', ('python2', 'python3'))
+def test_pkgutil_non_clashing_levels(session, interpreter):
+    session.interpreter = interpreter
+    session.install('-r', 'tests/requirements.txt')
+    session.install('pkgutil/a/', 'pkgutil/b-c/')
+    session.run('pytest', 'tests/test_a.py', 'tests/test_c.py')
