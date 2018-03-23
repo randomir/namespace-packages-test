8a5f468bd6993a4bf0daf55877f636397c57b50a
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-23 06:33:36 -0700

    Parametrize nox tests over ns pkg approaches

--- nox.py
+++ nox.py
@@ -1,61 +1,48 @@
 import nox
 
+def prepend_to_all(prefix, items):
+    return [prefix + item for item in items]
+
 
 @nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_pkgutil_same_level_only(session, interpreter):
+def test_same_level_only(session, approach, interpreter):
     """Base case: `root.a` and `root.b`."""
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
-    session.install('pkgutil/a/', 'pkgutil/b/')
+    session.install(*prepend_to_all(approach, ['/a/', '/b/']))
     session.run('pytest', 'tests/test_a.py', 'tests/test_b.py')
 
 
 @nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_pkgutil_non_clashing_levels(session, interpreter):
+def test_non_clashing_levels(session, approach, interpreter):
     """Base case: `root.a` and `root.b.c`."""
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
-    session.install('pkgutil/a/', 'pkgutil/b-c/')
+    session.install(*prepend_to_all(approach, ['/a/', '/b-c/']))
     session.run('pytest', 'tests/test_a.py', 'tests/test_b-c.py')
 
 
 @nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_native__mixed_levels(session, interpreter):
+def test_mixed_levels(session, approach, interpreter):
     """`root.b` from `b` package overlaps with `root.b.c` from `b-c`. We're adding
     `root.a` here as a baseline case (it shouldn't overlap with `root.b`)."""
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
-    session.install('native/a/', 'native/b/', 'native/b-c/')
+    session.install(*prepend_to_all(approach, ['/a/', '/b/', '/b-c/']))
     session.run('pytest', 'tests/test_a.py', 'tests/test_b.py', 'tests/test_b-c.py')
 
 
 @nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_native__mixed_levels__mixed_module_def(session, interpreter):
+def test_mixed_levels__mixed_module_def(session, approach, interpreter):
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
-    session.install('native/b/', 'native/b-c/', 'native/b-de/')
-    session.run('pytest', 'tests/test_b.py', 'tests/test_b-c.py', 'tests/test_b-de.py')
-
-
-@nox.session
-@nox.parametrize('interpreter', ('python2', 'python3'))
-def test_pkgutil_mixed_levels(session, interpreter):
-    """`root.b` from `b` package overlaps with `root.b.c` from `b-c`. We're adding
-    `root.a` here as a baseline case (it shouldn't overlap with `root.b`)."""
-    session.interpreter = interpreter
-    session.install('-r', 'tests/requirements.txt')
-    session.install('pkgutil/a/', 'pkgutil/b/', 'pkgutil/b-c/')
-    session.run('pytest', 'tests/test_a.py', 'tests/test_b.py', 'tests/test_b-c.py')
-
-
-@nox.session
-@nox.parametrize('interpreter', ('python2', 'python3'))
-def test_pkgutil__mixed_levels__mixed_module_def(session, interpreter):
-    session.interpreter = interpreter
-    session.install('-r', 'tests/requirements.txt')
-    session.install('native/b/', 'native/b-c/', 'native/b-de/')
+    session.install(*prepend_to_all(approach, ['/b/', '/b-c/', '/b-de/']))
     session.run('pytest', 'tests/test_b.py', 'tests/test_b-c.py', 'tests/test_b-de.py')
