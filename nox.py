a88eba56875ea058e1b9b2725f9740c418aed4e4
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-23 06:57:57 -0700

    Nox tests renamed, documented

--- nox.py
+++ nox.py
@@ -7,7 +7,7 @@ def prepend_to_all(prefix, items):
 @nox.session
 @nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_same_level_only(session, approach, interpreter):
+def same_level_only(session, approach, interpreter):
     """Base case: `root.a` and `root.b`."""
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
@@ -18,7 +18,7 @@ def test_same_level_only(session, approach, interpreter):
 @nox.session
 @nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_non_clashing_levels(session, approach, interpreter):
+def non_clashing_levels(session, approach, interpreter):
     """Base case: `root.a` and `root.b.c`."""
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
@@ -29,9 +29,14 @@ def test_non_clashing_levels(session, approach, interpreter):
 @nox.session
 @nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_mixed_levels(session, approach, interpreter):
-    """`root.b` from `b` package overlaps with `root.b.c` from `b-c`. We're adding
-    `root.a` here as a baseline case (it shouldn't overlap with `root.b`)."""
+def mixed_levels(session, approach, interpreter):
+    """`root` in `b` is a clean namespace package, but `root.b` is not (`b` is
+    not a 'stem', but a 'leaf'). I.e., `root.b` from `b` package overlaps with
+    `root.b.c` from `b-c`.
+
+    We're adding `root.a` here as a baseline case (it shouldn't overlap with
+    `root.b`).
+    """
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
     session.install(*prepend_to_all(approach, ['/a/', '/b/', '/b-c/']))
@@ -41,7 +46,11 @@ def test_mixed_levels(session, approach, interpreter):
 @nox.session
 @nox.parametrize('approach', ('pkgutil', 'native'))
 @nox.parametrize('interpreter', ('python2', 'python3'))
-def test_mixed_levels__mixed_module_def(session, approach, interpreter):
+def mixed_levels__mixed_module_def(session, approach, interpreter):
+    """Like `mixed_levels`, but with additional package that mixes module
+    definition types: `b-de` package defined `root.b.d` with file (``root/b/d.py``),
+    and `root.b.e` with dir/init (``root/b/e/__init__.py``).
+    """
     session.interpreter = interpreter
     session.install('-r', 'tests/requirements.txt')
     session.install(*prepend_to_all(approach, ['/b/', '/b-c/', '/b-de/']))
