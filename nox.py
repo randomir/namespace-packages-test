901b64a3fc5ac45f7ffe8cfb658d0c10a9d18246
Radomir Stevanovic <radomir@dwavesys.com>
2018-03-23 19:18:48 -0700

    Add uninstall tests

--- nox.py
+++ nox.py
@@ -10,10 +10,13 @@ install_commands = (
     ('python', 'setup.py', 'install'),
     ('python', 'setup.py', 'develop'))
 
-
-def prepend_to_all(prefix, items):
+def prepend(prefix, items):
     return [prefix + item for item in items]
 
+def init_session(session, interpreter):
+    """Common `session` init for all tests."""
+    session.interpreter = interpreter
+    session.install('-r', 'tests/requirements.txt')
 
 def install_packages(session, package_paths, install_command):
     """Install all packages given in `package_paths` in `session`, using
@@ -25,11 +28,10 @@ def install_packages(session, package_paths, install_command):
         session.run(*install_command)
         session.chdir(basedir)
 
-
-def init_session(session, interpreter):
-    """Common `session` init for all tests."""
-    session.interpreter = interpreter
-    session.install('-r', 'tests/requirements.txt')
+def uninstall_packages(session, package_names):
+    for pkg in package_names:
+        # in latest pip (~9), uninstall works the same for types of installs
+        session.run('pip', 'uninstall', '-y', pkg)
 
 
 @nox.session
@@ -39,8 +41,21 @@ def init_session(session, interpreter):
 def same_level_only(session, approach, interpreter, install_command):
     """Base case: `root.a` and `root.b`."""
     init_session(session, interpreter)
-    install_packages(session, prepend_to_all(approach, ['/a/', '/b/']), install_command)
+    install_packages(session, prepend(approach, ['/a/', '/b/']), install_command)
     session.run('pytest', 'tests/test_a.py', 'tests/test_b.py')
+
+@nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
+@nox.parametrize('interpreter', ('python2', 'python3'))
+@nox.parametrize('install_command', install_commands)
+def uninstall__same_level_only(session, approach, interpreter, install_command):
+    """Base case: install `root.a` and `root.b`, then uninstall one, and test
+    the other still works.
+    """
+    init_session(session, interpreter)
+    install_packages(session, prepend(approach, ['/a/', '/b/']), install_command)
+    uninstall_packages(session, ['root-a'])
+    session.run('pytest', 'tests/test_b.py')
 
 
 @nox.session
@@ -50,8 +65,19 @@ def same_level_only(session, approach, interpreter, install_command):
 def non_clashing_levels(session, approach, interpreter, install_command):
     """Base case: `root.a` and `root.b.c`."""
     init_session(session, interpreter)
-    install_packages(session, prepend_to_all(approach, ['/a/', '/b-c/']), install_command)
+    install_packages(session, prepend(approach, ['/a/', '/b-c/']), install_command)
     session.run('pytest', 'tests/test_a.py', 'tests/test_b-c.py')
+
+@nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
+@nox.parametrize('interpreter', ('python2', 'python3'))
+@nox.parametrize('install_command', install_commands)
+def uninstall__non_clashing_levels(session, approach, interpreter, install_command):
+    """Install `root-a`, `root-b-c`, then remove `root-a` and test `root-b-c`."""
+    init_session(session, interpreter)
+    install_packages(session, prepend(approach, ['/a/', '/b-c/']), install_command)
+    uninstall_packages(session, ['root-a'])
+    session.run('pytest', 'tests/test_b-c.py')
 
 
 @nox.session
@@ -67,8 +93,18 @@ def mixed_levels(session, approach, interpreter, install_command):
     `root.b`).
     """
     init_session(session, interpreter)
-    install_packages(session, prepend_to_all(approach, ['/a/', '/b/', '/b-c/']), install_command)
+    install_packages(session, prepend(approach, ['/a/', '/b/', '/b-c/']), install_command)
     session.run('pytest', 'tests/test_a.py', 'tests/test_b.py', 'tests/test_b-c.py')
+
+@nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
+@nox.parametrize('interpreter', ('python2', 'python3'))
+@nox.parametrize('install_command', install_commands)
+def uninstall__mixed_levels(session, approach, interpreter, install_command):
+    init_session(session, interpreter)
+    install_packages(session, prepend(approach, ['/a/', '/b/', '/b-c/']), install_command)
+    uninstall_packages(session, ['root-b'])
+    session.run('pytest', 'tests/test_a.py', 'tests/test_b-c.py')
 
 
 @nox.session
@@ -81,5 +117,15 @@ def mixed_levels__mixed_module_def(session, approach, interpreter, install_comma
     and `root.b.e` with dir/init (``root/b/e/__init__.py``).
     """
     init_session(session, interpreter)
-    install_packages(session, prepend_to_all(approach, ['/b/', '/b-c/', '/b-de/']), install_command)
+    install_packages(session, prepend(approach, ['/b/', '/b-c/', '/b-de/']), install_command)
     session.run('pytest', 'tests/test_b.py', 'tests/test_b-c.py', 'tests/test_b-de.py')
+
+@nox.session
+@nox.parametrize('approach', ('pkgutil', 'native'))
+@nox.parametrize('interpreter', ('python2', 'python3'))
+@nox.parametrize('install_command', install_commands)
+def uninstall__mixed_levels__mixed_module_def(session, approach, interpreter, install_command):
+    init_session(session, interpreter)
+    install_packages(session, prepend(approach, ['/b/', '/b-c/', '/b-de/']), install_command)
+    uninstall_packages(session, ['root-b-c'])
+    session.run('pytest', 'tests/test_b.py', 'tests/test_b-de.py')
